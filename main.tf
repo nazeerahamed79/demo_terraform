6342d91b6f90a34d70e18bb6593949d2bb87f17e
Sheik Basha <sk.saddam007@gmail.com>
2018-10-11 12:42:57 +0530

    sample changes 1

--- main.tf
+++ main.tf
@@ -118,9 +118,9 @@ resource "aws_elb" "example" {
   }
 }
 
-#output "public_ip" {
-#  value = "${aws_instance.web.public_ip}"
-#}
+// output "public_ip" {
+//   value = "${aws_instance.web.public_ip}"
+// }
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
 }
